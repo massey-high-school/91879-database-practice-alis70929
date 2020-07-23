7a5ded02d03863b55faa8708d359a32d387ad378
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-07-23 12:20:16 +1200

    Added nice colours

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Put Content Here">
-    <meta name="keywords" content="Put keywords here">
-    <meta name="author" content="Put your name here">
+    <meta name="description" content="Book Review Database">
+    <meta name="keywords" content="books, reading, fiction, non-fiction, genre, reviews">
+    <meta name="author" content="Shazeel Ali 11HCV">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Website Title goes here</title>
+    <title>Shazeels Book Review Database</title>
     
     <!-- Edit the link below / replace with your chosen google font -->
     <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
@@ -35,18 +35,11 @@
             </div>    <!-- / logo -->
         </a>
             
-            <h1>Website Name</h1>
+            <h1>Shazeels Book Review Database</h1>
         </div>    <!-- / banner -->
 
-        <!-- Navigation goes here.  Edit BOTH the file name and the link name -->
-        <div class="box nav">
-            <a href="index.html">Home</a> | 
-            <a href="page1.html">Page 1</a> | 
-            <a href="page2.html">Page 2</a> | 
-            <a href="page3.html">Page 3</a> | 
-            <a href="contact.html">Contact</a>
-        </div>    <!-- / nav -->        
-        
+       
+       
         <div class="box main">
             <h2>First Page heading</h2>
             
