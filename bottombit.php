0cf425574923f206a09c1e6cd6ccef4c2eda03a8
alis70929 <60583162+alis70929@users.noreply.github.com>
2020-07-24 10:55:19 +1200

    Started using php

---
+++ bottombit.php
@@ -0,0 +1,7 @@
+        <div class="box footer">
+            CC Shazeel Ali 2020
+        </div>    <!-- / footer -->
+    
+    </div>  <!-- / wrapper  -->
+    
+</body>    
--- index.php
+++ index.php
@@ -1,49 +1,6 @@
-<!DOCTYPE HTML>
-
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Book Review Database">
-    <meta name="keywords" content="books, reading, fiction, non-fiction, genre, reviews">
-    <meta name="author" content="Shazeel Ali 11HCV">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Shazeels Book Review Database</title>
-    
-    <!-- Edit the link below / replace with your chosen google font -->
-    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
-    
-    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
-    <link rel="stylesheet" href="css/no_side.css"> 
-    
-</head>
-    
-<body>
-    
-    <div class="wrapper">
-    
-
-        
-        <div class="box banner">
-            
-        <!-- logo image linking to home page goes here -->
-        <a href="index.php">
-            <div class="box logo"  title="Logo - Click here to go to the Home Page">
-            <img class="img-circle" src="images/gen_logo.png" width="150" height="150" alt="generic logo" />
-            
-            </div>    <!-- / logo -->
-        </a>
-            
-            <h1>Shazeels Book Review Database</h1>
-        </div>    <!-- / banner -->
-
-       <div class="box side"> 
-        
-           <h1>Sidebar</h1>
-           
-           
-        </div>
+<?php 
+include "topbit.php";
+?>
        
         <div class="box main">
             <h2>Books</h2>
@@ -61,12 +18,8 @@
             </p>
             
         </div>    <!-- / main -->
-        
+<?php 
+include "bottombit.php";
+?>
 
-        <div class="box footer">
-            CC Shazeel Ali 2020
-        </div>    <!-- / footer -->
     
-    </div>  <!-- / wrapper  -->
-    
-</body>        
